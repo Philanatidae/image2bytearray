546436e1d0666ae7ff4509ad7bf0b0f8b73958d9
Philip Rader <pjrader1@yahoo.com>
2022-02-08 19:21:37 -0700

    Add `clap` library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(last = true, required = true)]
+    image_path: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = Args::parse();
 }
